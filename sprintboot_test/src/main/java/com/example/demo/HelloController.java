7ac4e2ddfc7f89daf77c309ff0ba7862f2fdf01e
sgp0719@126.com <Administrator@10.6.220.203>
2020-06-11 18:30:43 +0800

    添加 springweb

--- sprintboot_test/src/main/java/com/example/demo/HelloController.java
+++ sprintboot_test/src/main/java/com/example/demo/HelloController.java
@@ -1,10 +1,14 @@
 package com.example.demo;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HelloController {
 
-	public void sayHello() {
-		System.out.println("aaaaaaaaaa");
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "learn spring boot";
 	}
 	
 }
