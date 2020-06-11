19121b0e5c9ad97b442e19c273546b32dc152ae2
sgp0719@126.com <Administrator@10.6.220.203>
2020-06-11 18:39:02 +0800

    添加测试的类和jar包

--- sprintboot_test/src/test/java/com/example/demo/SprintbootTestApplicationTests.java
+++ sprintboot_test/src/test/java/com/example/demo/SprintbootTestApplicationTests.java
@@ -1,13 +1,31 @@
 package com.example.demo;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 @SpringBootTest
 class SprintbootTestApplicationTests {
 
+	private MockMvc mvc;
+	
+	@BeforeAll
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new HelloController()).build();
+    }
+	
 	@Test
-	void contextLoads() {
+	public void getHello() throws Exception {
+		 mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
+         .andExpect(MockMvcResultMatchers.status().isOk())
+         .andDo(MockMvcResultHandlers.print())
+         .andReturn();
 	}
 
 }
